af1c5894e0e1261c36420cd71150960b3ecbaa34
Spirch <you@example.com>
2023-09-30 20:35:38 -0400

    some cleanup on the sqlite views

--- esphomecsharp/EspHomeContext.cs
+++ esphomecsharp/EspHomeContext.cs
@@ -63,9 +63,9 @@ namespace esphomecsharp
 
                 await test.Database.ExecuteSqlRawAsync("PRAGMA journal_mode=DELETE");
 
-                await test.Database.ExecuteSqlRawAsync("CREATE VIEW MinMaxValue as  \r\nSELECT row.Name \r\n        , row.FriendlyName \r\n        , data.MaxValue   \r\n        , data.MinValue   \r\n        , row.Unit   \r\n   FROM [RowEntry] row   \r\n   inner join    \r\n   (   \r\n    SELECT [RowEntryId]   \r\n        ,max([Data]) MaxValue   \r\n        ,min([Data]) MinValue   \r\n    FROM [Event]   \r\n    group by RowEntryId   \r\n   ) data on data.[RowEntryId] = row.[RowEntryId]   \r\n order by row.Unit, row.FriendlyName");
+                await test.Database.ExecuteSqlRawAsync("CREATE VIEW MinMaxValue as  \r\nSELECT  row.Name \r\n      , row.FriendlyName \r\n      , data.MaxValue \r\n      , data.MinValue \r\n      , row.Unit \r\nFROM [RowEntry] row \r\nINNER join \r\n( \r\n    SELECT  [RowEntryId] \r\n          , max([Data]) MaxValue\r\n          , min([Data]) MinValue\r\n    FROM [Event] \r\n    GROUP BY RowEntryId \r\n) data ON data.[RowEntryId] = row.[RowEntryId] \r\nORDER BY row.Unit, row.FriendlyName");
 
-                await test.Database.ExecuteSqlRawAsync("CREATE VIEW ShowAll as  \r\n  SELECT datetime(data.UnixTime, 'unixepoch', 'localtime') DateTime \r\n       , data.UnixTime  \r\n       , row.Name   \r\n       , row.FriendlyName   \r\n       , data.Data   \r\n       , row.Unit   \r\n  FROM [RowEntry] row   \r\n  inner join [Event] data on data.[RowEntryId] = row.[RowEntryId]   \r\n  order by row.FriendlyName, row.Name, data.UnixTime ");
+                await test.Database.ExecuteSqlRawAsync("CREATE VIEW ShowAll as  \r\nSELECT    datetime(data.UnixTime, 'unixepoch', 'localtime') DateTime \r\n         , date(data.UnixTime, 'unixepoch', 'localtime') Date \r\n         , time(data.UnixTime, 'unixepoch', 'localtime') Time \r\n         , data.UnixTime \r\n         , row.Name \r\n         , row.FriendlyName \r\n         , data.Data \r\n         , row.Unit \r\nFROM [RowEntry] row \r\nINNER join [Event] data ON data.[RowEntryId] = row.[RowEntryId] \r\nORDER BY row.FriendlyName, row.Name, data.UnixTime");
 
                 await test.Database.CloseConnectionAsync();
             }
